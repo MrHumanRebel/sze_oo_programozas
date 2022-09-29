6b46e8a1d5b8618ae726166e48de628c381793b0
Dániel Székely <szekelydani5g@gmail.com>
2022-09-29 19:37:38 +0200

    update 1.1

--- anyagok/youtube/skandi/main.cpp
+++ anyagok/youtube/skandi/main.cpp
@@ -9,6 +9,7 @@ int fileread(int array[7][3000], string filename)
 {
     int i = 0;
     int ugras = 0;
+    int total = 0;
 
     ifstream file(filename);
     string aktSor;
@@ -19,10 +20,11 @@ int fileread(int array[7][3000], string filename)
         {
             for (long unsigned int j = 0; j < aktSor.length(); j += 2)
             {
-                if (ugras == 6)
+                if (ugras == 7)
                 {
+                    cout << endl;
                     ugras = 0;
-                    i -= 5;
+                    i -= 6;
                 }
                 // cout << aktSor << endl;
                 string akt = "\0";
@@ -34,10 +36,11 @@ int fileread(int array[7][3000], string filename)
                 }
                 array[ugras][i] = stoi(akt);
                 cout << array[ugras][i] << '\t';
-                // cout << "Hely: " << ugras << "Hely2: " << i << endl;
+                //    cout << "Hely: " << ugras << "Hely2: " << i << endl;
 
                 ugras++;
                 i++;
+                total++;
             }
             cout << endl;
         }
@@ -46,28 +49,29 @@ int fileread(int array[7][3000], string filename)
     {
         cout << "File read error!" << endl;
     }
-    return i;
+    return total;
 }
 
 void counter(int numbers[7][3000], int appearance[7][35][2], int size)
 {
-    int id = 0;
 
     for (int i = 1; i <= 35; i++)
     {
+        int id = 0;
         for (int k = 0; k < 7; k++)
         {
-            for (int j = 0; j < size; j++)
+            for (int j = 0; j <= size; j++)
             {
                 if (numbers[k][j] == i)
                 {
                     appearance[k][id][0] = i;
                     appearance[k][id][1]++;
-                    //cout << "Appearence of " << i << " in position " << k << " in data: " << appearance[k][id][1] << endl;
                 }
             }
+            if (appearance[k][id][1] != 0)
+                cout << "Appearence of " << i << " in position " << k + 1 << " in data: " << appearance[k][id][1] << " db" << endl;
+            id++;
         }
-        id++;
     }
 }
 
@@ -98,13 +102,18 @@ int main()
     }
 
     // Read file and get the number of draws
-    int total_draws = fileread(numbers, "/home/szeke/projects/sze_oo_programozas/anyagok/youtube/skandi/data.txt");
-    int total_picks = total_draws / 3;
+    int total_draws = fileread(numbers, "/home/szeke/projects/sze_oo_programozas/anyagok/youtube/skandi/testdata.txt");
+    int total_picks = total_draws / 7;
+
+    /*for (int i = 0; i < total_draws; i++)
+    {
+        cout << numbers[0][i] << endl;
+    }*/
 
     cout << "Total draws: " << total_draws << endl;
     cout << "Total picks: " << total_picks << endl;
 
-    counter(numbers, appearance, total_draws);
+    counter(numbers, appearance, total_picks);
 
     /*
     for (int i = 0; i < num_choices; i++)
