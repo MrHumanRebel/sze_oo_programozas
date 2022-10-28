2095c85205801e26f5ee366acfd6ad8be062d664
Dániel Székely <szekelydani5g@gmail.com>
2022-10-28 15:11:12 +0200

    update YT

--- anyagok/youtube/pointers/function_pointers/main.cpp
+++ anyagok/youtube/pointers/function_pointers/main.cpp
@@ -1,7 +1,106 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int getNum()
+{
+    return 5;
+}
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+bool ascComp(int a, int b)
+{
+    return a < b;
+}
+bool descComp(int a, int b)
+{
+    return a > b;
+}
+void sortAsc(vector<int> &numbersVector)
+{
+    for (long unsigned int startIndex = 0; startIndex < numbersVector.size(); startIndex++)
+    {
+        int bestIndex = startIndex;
+        for (long unsigned int currentIndex = startIndex + 1; currentIndex < numbersVector.size(); currentIndex++)
+        {
+            if (ascComp(numbersVector[currentIndex], numbersVector[bestIndex]))
+                bestIndex = currentIndex;
+        }
+        swap(numbersVector[startIndex], numbersVector[bestIndex]);
+    }
+}
+void sortDesc(vector<int> &numbersVector)
+{
+    for (long unsigned int startIndex = 0; startIndex < numbersVector.size(); startIndex++)
+    {
+        int bestIndex = startIndex;
+        for (long unsigned int currentIndex = startIndex + 1; currentIndex < numbersVector.size(); currentIndex++)
+        {
+            if (descComp(numbersVector[currentIndex], numbersVector[bestIndex]))
+                bestIndex = currentIndex;
+        }
+        swap(numbersVector[startIndex], numbersVector[bestIndex]);
+    }
+}
+
+void printNum(vector<int> &numbersVector)
+{
+    for (long unsigned int i = 0; i < numbersVector.size(); i++)
+    {
+        cout << numbersVector[i] << " ";
+    }
+    cout << endl;
+}
+
+void sortCustom(vector<int> &numbersVector, bool (*compareFuncPtr)(int, int)) // Receives also a function pointer
+{
+    for (long unsigned int startIndex = 0; startIndex < numbersVector.size(); startIndex++)
+    {
+        int bestIndex = startIndex;
+        for (long unsigned int currentIndex = startIndex + 1; currentIndex < numbersVector.size(); currentIndex++)
+        {
+            if (compareFuncPtr(numbersVector[currentIndex], numbersVector[bestIndex]))
+                bestIndex = currentIndex;
+        }
+        swap(numbersVector[startIndex], numbersVector[bestIndex]);
+    }
+}
 
 int main()
 {
+    int (*funcPtr)() = getNum;
+    cout << funcPtr() << endl;
+
+    cout << "---------------" << endl;
+
+    int (*funcPtr2)(int, int) = Sum;
+    cout << funcPtr2(876, 923) << endl;
+
+    cout << "---------------" << endl;
+
+    vector<int> myNumbers = {2, 5, 1, 3, 6, 4};
+    printNum(myNumbers);
+    sortAsc(myNumbers);
+    printNum(myNumbers);
+    sortDesc(myNumbers);
+    printNum(myNumbers);
+
+    cout << "---------------" << endl;
+
+    vector<int> myNumbers2 = {21, 59, 11, 35, 69, 444};
+    printNum(myNumbers2);
+    sortCustom(myNumbers2, descComp);
+    printNum(myNumbers2);
+    sortCustom(myNumbers2, ascComp);
+    printNum(myNumbers2);
+
+    bool (*funcPtr3)(int, int) = descComp;
+    sortCustom(myNumbers2, funcPtr3);
+    printNum(myNumbers2);
+
     return 0;
 }
--- anyagok/youtube/pointers/smart_pointers/main.cpp
+++ anyagok/youtube/pointers/smart_pointers/main.cpp
@@ -17,6 +17,7 @@ public:
 
 int main()
 {
+    // Normal Smart PTR
     unique_ptr<int> unPtr1 = make_unique<int>(25); // Create unique pointer to int, and assigned 25 to it
     cout << "Adress: " << &unPtr1 << endl;         // Get adress
     cout << "Value: " << *unPtr1 << endl;          // Get value
@@ -30,6 +31,24 @@ int main()
     cout << "----------------" << endl;
 
     unique_ptr<MyClass> unPtr3 = make_unique<MyClass>();
+
+    cout << "----------------" << endl;
+    // Shared Smart PTR
+
+    shared_ptr<MyClass> shPtr1 = make_shared<MyClass>();    // Shared PTR
+    cout << "Shared count: " << shPtr1.use_count() << endl; // Give number of owners
+    shared_ptr<MyClass> shPtr2 = shPtr1;
+    cout << "Shared count: " << shPtr1.use_count() << endl;
+
+    cout << "----------------" << endl;
+
+    // Weak Smart PTR
+
+    weak_ptr<int> wPtr1;
+    {
+        shared_ptr<int> shPtr4 = make_shared<int>(50);
+        wPtr1=shPtr4;
+    }
 
     return 0;
 }
