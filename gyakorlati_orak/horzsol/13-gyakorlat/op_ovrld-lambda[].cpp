b2888d1d77099c6b362366cbdf357916bcbac5bf
MrHumanRebel <szekelydani5g@gmail.com>
2022-11-28 07:08:13 +0100

    add gyak

--- gyakorlati_orak/horzsol/13-gyakorlat/op_ovrld-lambda[].cpp
+++ gyakorlati_orak/horzsol/13-gyakorlat/op_ovrld-lambda[].cpp
@@ -1,212 +1,288 @@
-op_ovrld/1
-/***************/
+op_ovrld
+/********* Decl.hpp ************/
 #include <iostream>
-using namespace std;
 
-class tort 
- {
-   private:
-    int szam, nev;  
-   public:
-    tort operator*(tort m_t) {
-     return tort(szam * m_t.getSzam(), nev * m_t.getNev()); }
-    tort operator*(int i) { return tort(szam * i, nev); }
-    int getSzam() const { return szam; }
-    int getNev() const { return nev; }
-    tort(int sz, int nv) { szam=sz; nev=nv; }  
-	~tort() { }                        
- };
+    using namespace std;
+
+class tort
+{
+  int szaml, nevz;                      // másolatok az objektumok mem. területein
+  void setSzaml(int sz) { szaml = sz; } // szaml=szaml => this->szaml=szaml
+  void setNevz(int nv) { nevz = nv; }   // nevz=nevz => this->nevz=nevz
+public:                                 // a metódusok egyediek, minden objektum ezeket használja => *this
+  tort(int sz, int nv)
+  {
+    setSzaml(sz);
+    setNevz(nv);
+  }
+  int getSzaml() const { return szaml; }
+  int getNevz() const { return nevz; }
+  tort operator*(tort m_t)
+  {
+    return tort(szaml * m_t.getSzaml(), nevz * m_t.getNevz());
+  }
+  /*    tort& operator*=(tort m_t) {
+                     tort e_t = *this * m_t;
+                     return *this = e_t; }*/
+  //    tort operator*(int i) { return tort(szaml * i, nevz); }
+  /*    tort& operator*=(int i) {
+                     tort e_t = *this * i;
+                     return *this = e_t; }*/
+  ~tort() {}
+  //	  friend istream& operator>>(istream& be, tort& t);
+};
 
 /*tort operator*(tort e_t, tort m_t) {
-    return tort(e_t.getSzam() * m_t.getSzam(),
-                e_t.getNev() * m_t.getNev()); }*/
+    return tort(e_t.getSzaml() * m_t.getSzaml(),
+                e_t.getNevz() * m_t.getNevz()); }*/
+
+/*tort& tort::operator*=(tort m_t) {
+                   tort e_t = *this * m_t;
+                   return *this = e_t; }*/
 
 /*tort operator*(tort e_t, int i) {
-    return tort(e_t.getSzam() * i,
-                e_t.getNev()); }*/
+    return tort(e_t.getSzaml() * i,
+                e_t.getNevz()); }*/
 
-tort operator*(int i, tort m_t) {
-    return tort(i * m_t.getSzam(),
-                m_t.getNev()); }
+/*tort& tort::operator*=(int i) {
+                   tort e_t = *this * i;
+                   return *this = e_t; }*/
 
-ostream& operator<<(ostream& ki, const tort& t) {
-  ki << t.getSzam() << "/" << t.getNev();
-  return ki;
-}
+/*tort operator*(int i, tort m_t) {
+    return tort(i * m_t.getSzaml(),
+                m_t.getNevz()); }*/
 
-int main(void)
- {
-  tort t1(2, 3);
-  tort t2(4, 5);
-  tort t3 = t1 * t2;
-  //tort t3 = t1.operator*(t2);
-  cout << "\nAlap tört: " << t3.getSzam() << "/" << t3.getNev() << endl;
-  cout << "Túlterhelt tört: " << t3 << "\n" << endl;
-  int i = 3 * 4;
-  tort t4 = t1 * /*2*/ i;
-  cout << "Alap egész: " << i << endl;
-  cout << "Túlterhelt tagfgv. egész: " << t4 << "\n" << endl;
-  tort t5 = /*2*/ i * t1;
-  cout << "Túlterhelt nem tagfgv. egész: " << t5 << "\n" << endl;
-  cout << t1 << " - " << t2 << " - " << t3 << " - " << t4 << " - " << t5 << endl;
-  return 0;
-}
-/***************************************************************************************/
-op_ovrld/2
-/***************/
-#include <iostream>
-using namespace std;
+/*ostream& operator<<(ostream& ki, const tort& t) {
+  return ki << t.getSzaml() << "/" << t.getNevz(); }*/
 
-class tort 
- {
-   private:
-    int szam, nev;  
-   protected:
-    int getSzam() const { return szam; }
-    int getNev() const { return nev; }
-   public:
-    tort operator*(tort m_t) {
-     return tort(szam * m_t.getSzam(), nev * m_t.getNev()); }
-    tort operator*(int i) { return tort(szam * i, nev); }
-    tort(int sz, int nv) { szam=sz; nev=nv; } 
-	~tort() { }    
-   friend tort operator*(int i, tort m_t);
-   friend ostream& operator<<(ostream& ki, const tort& t);              
- };
+/*istream& operator>>(istream& be, tort& t) {
+  cout << "\nKérem a tört számlálóját: "; be >> t.szaml;
+  cout << "  Kérem a tört nevezőjét: "; be >> t.nevz;
+  return be; }*/
 
-/*tort operator*(tort e_t, tort m_t) {
-    return tort(e_t.getSzam() * m_t.getSzam(),
-                e_t.getNev() * m_t.getNev()); }*/
-
-/*tort operator*(tort e_t, int i) {
-    return tort(e_t.getSzam() * i,
-                e_t.getNev()); }*/
-
-tort operator*(int i, tort m_t) {
-    return tort(i * m_t.getSzam(),
-                m_t.getNev()); }
-
-ostream& operator<<(ostream& ki, const tort& t) {
-  ki << t.getSzam() << "/" << t.getNev();
-  return ki;
-}
+/********* main.cpp ************/
+#include "Decl.hpp"
 
 int main(void)
- {
+{
   tort t1(2, 3);
   tort t2(4, 5);
-  tort t3 = t1 * t2;
-  //tort t3 = t1.operator*(t2);
-  //cout << "\nAlap tört: " << t3.getSzam() << "/" << t3.getNev() << endl;
-  cout << "Túlterhelt tört: " << t3 << "\n" << endl;
-  int i = 3 * 4;
-  tort t4 = t1 * /*2*/ i;
-  cout << "Alap egész: " << i << endl;
-  cout << "Túlterhelt tagfgv. egész: " << t4 << "\n" << endl;
-  tort t5 = /*2*/ i * t1;
-  cout << "Túlterhelt nem tagfgv. egész: " << t5 << "\n" << endl;
-  cout << t1 << " - " << t2 << " - " << t3 << " - " << t4 << " - " << t5 << endl;
+  // cin >> t1 >> t2; // -2/6 és 8/-5
+  tort t3 = t1 * t2; // ambiguous esete
+  // tort t3 = t1.operator*(t2); // egyszerre mindkettő
+  // t3 = operator*(t1, t3); // egyszerre mindhárom
+  cout << "\n Alap kiírása a törtnek ('*'):\n\t\t t3 = "
+       << t3.getSzaml() << "/" << t3.getNevz() << endl;
+  /*  cout << "\n Túlterhelt kiírása a törteknek ('*');\n\t\t t1 = "\
+         << t1 << "\n\t\t t2 = " << t2 << "\n\t\t t3 = " << t3 << endl;
+    t3 *= t2;
+    cout << "\n Túlterhelt kiírása a törtnek ('*='):\n\t\t t3 = " << t3 << endl;
+    int i = 2 * 3;
+    cout << "\n Alap kiírása az egésznek:\n\t\t i = " << i << endl;
+    tort t4 = t1 * i; // *2 illetve 2 * t1!? és 2 * t1 * 2
+    cout << "\n Túlterhelt 'operator*' egész szorzással:\n\t\t t4 = "\
+         << t4 << endl;
+    t4 *= 2;
+    cout << "\n Túlterhelt 'operator*=' egész szorzással:\n\t\t t4 = "\
+         << t4 << endl;
+    cout << "\n" << t1 << " és " << t2 << " és " << t3 << " és " << t4 << endl;*/
   return 0;
 }
+
 /***************************************************************************************/
-op_ovrld/3
-/***************/
+op_ovrld : eredmény(Fejlesztés
+                    : előjelek és 0 nevező kezelése !)
+/********* Decl.hpp ************/
 #include <iostream>
-using namespace std;
 
-class tort 
- {
-   private:
-    int szam, nev;  
-   protected:
-    int getSzam() const { return szam; }
-    int getNev() const { return nev; }
-    void setSzam(int szm) { szam=szm; }
-    void setNev(int nv) { nev=nv; }
-   public:
-   tort operator*(tort m_t) {
-    return tort(szam * m_t.getSzam(), nev * m_t.getNev());}
-   tort operator*(int i) { return tort(szam * i, nev); }
-   tort() { }
-   tort(int sz, int nv) { szam=sz; nev=nv; } 
-   ~tort() { }    
-   friend tort operator*(int i, tort m_t);
-   friend ostream& operator<<(ostream& ki, const tort& t);
-   friend istream& operator>>(istream& be, tort& t); 
- };
+               using namespace std;
 
-tort operator*(int i, tort m_t) {
-    return tort(i * m_t.getSzam(), m_t.getNev()); }
+class tort
+{
+  int szaml, nevz;
+  void setSzaml(int szaml) { this->szaml = szaml; }
+  void setNevz(int nevz) { this->nevz = nevz; }
 
-ostream& operator<<(ostream& ki, const tort& t) {
-  ki << t.getSzam() << "/" << t.getNev();
-  return ki;
+public:
+  tort(int szaml, int nevz)
+  {
+    setSzaml(szaml);
+    setNevz(nevz);
+  }
+  int getSzaml() const { return szaml; }
+  int getNevz() const { return nevz; }
+  //    tort operator*(tort m_t) {
+  //      return tort(szaml * m_t.getSzaml(), nevz * m_t.getNevz()); }
+  tort &operator*=(tort m_t); /* {
+                 tort e_t = *this * m_t;
+                 return *this = e_t; }*/
+                              //    tort operator*(int i) { return tort(szaml * i, nevz); }
+  tort &operator*=(int i);    /* {
+                    tort e_t = *this * i;
+                    return *this = e_t; }*/
+  ~tort() {}
+  friend istream &operator>>(istream &be, tort &t);
+};
+
+tort operator*(tort e_t, tort m_t)
+{
+  return tort(e_t.getSzaml() * m_t.getSzaml(),
+              e_t.getNevz() * m_t.getNevz());
 }
 
-istream& operator>>(istream& be, tort& t) {
+tort &tort::operator*=(tort m_t)
+{
+  tort e_t = *this * m_t;
+  return *this = e_t;
+}
+
+tort operator*(tort e_t, int i)
+{
+  return tort(e_t.getSzaml() * i,
+              e_t.getNevz());
+}
+
+tort &tort::operator*=(int i)
+{
+  tort e_t = *this * i;
+  return *this = e_t;
+}
+
+tort operator*(int i, tort m_t)
+{
+  return tort(i * m_t.getSzaml(),
+              m_t.getNevz());
+}
+
+ostream &operator<<(ostream &ki, const tort &t)
+{
+  return ki << t.getSzaml() << "/" << t.getNevz();
+}
+
+istream &operator>>(istream &be, tort &t)
+{
   cout << "\nKérem a tört számlálóját: ";
-  be >> t.szam;
-  cout << "Kérem a tört nevezőjét: ";
-  be >> t.nev;
+  be >> t.szaml;
+  cout << "  Kérem a tört nevezőjét: ";
+  be >> t.nevz;
   return be;
 }
 
+/********* main.cpp ************/
+#include "Decl.hpp"
+
 int main(void)
- {
-  tort t1, t2;
-  cin >> t1 >> t2;
-  tort t3 = t1 * t2;
-  cout << "\nTúlterhelt tört: " << t3 << "\n" << endl;
-  int i = 3 * 4;
-  tort t4 = t1 * i;
-  cout << "Alap egész: " << i << endl;
-  cout << "Túlterhelt tagfgv. egész: " << t4 << "\n" << endl;
-  tort t5 = i * t1;
-  cout << "Túlterhelt nem tagfgv. egész: " << t5 << "\n" << endl;
-  cout << t1 << " - " << t2 << " - " << t3 << " - " << t4 << " - " << t5 << endl;
+{
+  tort t1(2, 3);
+  tort t2(4, 5);
+  cin >> t1 >> t2;   // -2/6 és 8/-5
+  tort t3 = t1 * t2; // ambiguous esete
+  // tort t3 = t1.operator*(t2);
+  // t3 = operator*(t1, t3);
+  cout << "\n Alap kiírása a törtnek ('*'):\n\t\t t3 = "
+       << t3.getSzaml() << "/" << t3.getNevz() << endl;
+  cout << "\n Túlterhelt kiírása a törteknek ('*');\n\t\t t1 = "
+       << t1 << "\n\t\t t2 = " << t2 << "\n\t\t t3 = " << t3 << endl;
+  t3 *= t2;
+  cout << "\n Túlterhelt kiírása a törtnek ('*='):\n\t\t t3 = " << t3 << endl;
+  int i = 2 * 3;
+  cout << "\n Alap kiírása az egésznek:\n\t\t i = " << i << endl;
+  tort t4 = 2 * t1 * 2; // *2 illetve 2 * t1!? és 2 * t1 * 2
+  cout << "\n Túlterhelt 'operator*' egész szorzással:\n\t\t t4 = "
+       << t4 << endl;
+  t4 *= 2;
+  cout << "\n Túlterhelt 'operator*=' egész szorzással:\n\t\t t4 = "
+       << t4 << endl;
+  cout << "\n"
+       << t1 << " és " << t2 << " és " << t3 << " és " << t4 << endl;
   return 0;
 }
+
 /***************************************************************************************/
 [] - lambda kifejezés
 /********* Decl.hpp ************/
-using namespace std;
-
-vector<int> hpmp(3);
-/* komparátor/predikátum (összehasonlító/tulajdonság hozzárendelő) függvény */
- 
-/********* 1.cpp ************/
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+    using namespace std;
+
+vector<int> hpmp(3);
+
+// komparátor/predikátum (összehasonlító/tulajdonság hozzárendelő) függvények
+// megvalósítása; lambda kifejezésekkel.
+// [] - a lambda kifejezés, egy ideiglenes, névtelen függvény objektum
+
+/********* main.cpp ************/
 #include "Decl.hpp"
- 
+
 int main(void)
- {
-  hpmp[0]=-1; hpmp.push_back(6);
-  for(int i=0; i<10; i++)
-   { cout << hpmp[i] << " "; hpmp.push_back(7+i); }
-  cout << "\n"; hpmp.pop_back(); hpmp.resize(19); hpmp.push_back(20);
-  for(auto it = hpmp.begin(); it != hpmp.end(); it++)
-   { cout << *it << " "; } cout << "\n";
+{
+  hpmp[0] = -1;
+  hpmp.push_back(6);
+  for (int i = 0; i < 10; i++)
+  {
+    hpmp.push_back(7 + i);
+  }
+  hpmp.pop_back();
+  hpmp.resize(19);
+  hpmp.push_back(20);
+  for (auto it = hpmp.begin(); it != hpmp.end(); it++)
+  {
+    cout << (*it) << " ";
+  }
+  cout << "\n";
+  // lambda kifejezések, metódus és fgv. paraméterként
   sort(hpmp.begin(), hpmp.end(),
-    [](int i, int j) -> bool { return i>j; });
-  for(auto it = hpmp.begin(); it != hpmp.end(); it++)
-   { cout << *it << " "; } cout << "\n";
+       [](int i, int j) -> bool
+       { return i > j; });
+  cout << "\n";
+  for (auto it = hpmp.cbegin(); it != hpmp.cend(); it++)
+  {
+    cout << *it << " ";
+  }
+  cout << "\n";
   hpmp.erase((remove_if(hpmp.begin(), hpmp.end(),
-    [](int i) -> bool { return ((i%2)==1 || (i%2)==-1); })),hpmp.end());
-  for(int i=0; i<hpmp.size(); i++)
-   { cout << hpmp[i] << " "; } cout << "\n";
+                        [](int i) -> bool
+                        { return ((i % 2) == 1 or (i % 2) == -1); })),
+             hpmp.end());
+  for (auto it = hpmp.cbegin(); it != hpmp.cend(); it++)
+  {
+    cout << *it << " ";
+  }
+  cout << "\n";
+  cout << *max_element(hpmp.begin(), hpmp.end(),
+                       [](int i, int j) -> bool
+                       { return i < j; })
+       << "\n";
+  cout << *min_element(hpmp.begin(), hpmp.end(),
+                       [](int i, int j) -> bool
+                       { return i <= j; })
+       << "\n";
   cout << count_if(hpmp.begin(), hpmp.end(),
-   [](int i) -> bool { return ((i%2)==0 && i!=0); }) << "\n";
-  int ve = [](int x){ return x*x; }(5);
-  cout << ve << endl;
-  int v = [](int x){ return x*x; }
-  (count_if(hpmp.begin(), hpmp.end(),
-   [](int i) -> bool { return ((i%2)==0 && i!=0); }));
-  cout << v << endl;
-  int j=7, k=10;
-  cout << [=](int x){ return x*x+j; }(5) << "\n"
-  << j;  /*&,k ++j +k*/
+                   [](int i) -> bool
+                   { return ((i % 2) == 0 && i != 0); })
+       << "\n";
+  cout << "\n";
+  // önálló lambda kifejezések, a v.é. típusa a fordítóra bízva
+  cout << [](int x)
+  { return x * x; }(5)
+       << endl;
+  cout << [](int x)
+  { return x * x; }(count_if(hpmp.begin(), hpmp.end(),
+                             [](int i)
+                             { return ((i % 2) == 0 && i != 0); }))
+       << "\n\n";
+  // [=] érték ill. [&] referencia szerinti változó-átvétel
+  int j = 7, k = 10;
+  cout << [=](int x)
+  { return x * k + j; }(5)
+       << "\tj = " << j << "\tk = " << k << endl;
+  cout << [&](int x)
+  { return x * (k++) + (++j)++; }(5)
+       << "\tj = " << j << "\tk = " << k << endl;
+  //  cout << [=,&j](int x){ return x*(k++) + (++j)++; }(5)\
+//       << "\tj = " << j << "\tk = " << k << endl;
   return 0;
 }
 /***************************************************************************************/
